94213878c2acd039ac22f28664b1a27e11c3ad6f
AleksandrApoltsev <docapoltsev@yandex.ru>
2023-04-21 19:19:22 +0400

    fix

--- src/test/java/api/randomuser/test/random_user/RandomUserTest.java
+++ src/test/java/api/randomuser/test/random_user/RandomUserTest.java
@@ -3,6 +3,8 @@ package api.randomuser.test.random_user;
 import api.randomuser.dto.ResultsModel;
 import api.randomuser.steps.random_user.RandomUserSteps;
 import api.randomuser.test.BaseTest;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -10,12 +12,16 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.util.Map;
 
 import static api.randomuser.assertions.RandomUserAssertions.checkResponseRandomUser;
+import static io.qameta.allure.SeverityLevel.NORMAL;
+
 @DisplayName("Тестирование RandomUser")
 public class RandomUserTest extends BaseTest {
 
+    @Owner("Апольцев АС")
     @ParameterizedTest
     @MethodSource("api.randomuser.test_data.RandomUserTestData#checkRandomUser")
     @DisplayName("Проверка вывода рандомного пользователя")
+    @Severity(NORMAL)
     void checkRandomUser(Map<String, String> queryParam,
                          RandomUserSteps steps)
     {
--- src/test/java/ui/tests/AutomationPracticeFormPageObjectsTest.java
+++ src/test/java/ui/tests/AutomationPracticeFormPageObjectsTest.java
@@ -1,46 +1,53 @@
 package ui.tests;
 
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import ui.TestBase;
 import ui.pages.FormPage;
 import ui.test_data.TestData;
 
+import static io.qameta.allure.Allure.step;
+import static io.qameta.allure.SeverityLevel.NORMAL;
 
+@DisplayName("Тест")
 public class AutomationPracticeFormPageObjectsTest extends TestBase {
     FormPage formPage = new FormPage();
     TestData testData = new TestData();
 
     @Test
+    @Owner("Апольцев АС")
     @DisplayName("Проверка формы")
+    @Severity(NORMAL)
     void formTest() {
-        formPage.openPage()
-                .setFirstName(testData.getFirstName())
-                .setLastName(testData.getLastName())
-                .setUserEmail(testData.getEmail())
-                .setGender(testData.getGender())
-                .setUserNumber(testData.getNumber())
-                .setBirthDay(testData.getDay(), testData.getMonth(), testData.getYear())
-                .setSubjects(testData.getSubjects())
-                .setHobbiesWrapper(testData.getHobbies())
-                .setUploadPicture(testData.getPicture())
-                .setCurrentAddress(testData.getAddress())
-                .setState(testData.getState())
-                .setCity(testData.getCity())
-                .setSubmit();
+        step("Открываем страницу с формой, вводим тестовые данные", () ->
+                formPage.openPage()
+                        .setFirstName(testData.getFirstName())
+                        .setLastName(testData.getLastName())
+                        .setUserEmail(testData.getEmail())
+                        .setGender(testData.getGender())
+                        .setUserNumber(testData.getNumber())
+                        .setBirthDay(testData.getDay(), testData.getMonth(), testData.getYear())
+                        .setSubjects(testData.getSubjects())
+                        .setHobbiesWrapper(testData.getHobbies())
+                        .setUploadPicture(testData.getPicture())
+                        .setCurrentAddress(testData.getAddress())
+                        .setState(testData.getState())
+                        .setCity(testData.getCity())
+                        .setSubmit());
 
-        formPage.checkResultsTableVisible()
-                .checkResult("Student Name", testData.getFirstName() + " " + testData.getLastName())
-                .checkResult("Student Email", testData.getEmail())
-                .checkResult("Gender", testData.getGender())
-                .checkResult("Mobile", testData.getNumber())
-                .checkResult("Date of Birth", testData.getDay() + " " + testData.getMonth() + "," + testData.getYear())
-                .checkResult("Subjects", testData.getSubjects())
-                .checkResult("Hobbies", testData.getHobbies())
-                .checkResult("Picture", testData.getPicture())
-                .checkResult("Address", testData.getAddress())
-                .checkResult("State and City", testData.getState() + " " + testData.getCity());
-
+        step("Проверка ввода тестовых данных", () ->
+                formPage.checkResultsTableVisible()
+                        .checkResult("Student Name", testData.getFirstName() + " " + testData.getLastName())
+                        .checkResult("Student Email", testData.getEmail())
+                        .checkResult("Gender", testData.getGender())
+                        .checkResult("Mobile", testData.getNumber())
+                        .checkResult("Date of Birth", testData.getDay() + " " + testData.getMonth() + "," + testData.getYear())
+                        .checkResult("Subjects", testData.getSubjects())
+                        .checkResult("Hobbies", testData.getHobbies())
+                        .checkResult("Picture", testData.getPicture())
+                        .checkResult("Address", testData.getAddress())
+                        .checkResult("State and City", testData.getState() + " " + testData.getCity()));
     }
 }
